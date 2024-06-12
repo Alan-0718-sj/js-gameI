27502113fecc303ecca015f40805365f51008e93
Alan Silva <135849659+Alan-0718-sj@users.noreply.github.com>
2024-06-12 19:22:39 -0300

    Add files via upload

---
+++ desafios/desafio-hora-pratica.js
@@ -0,0 +1,22 @@
+
+alert('Bem-vindo(a) ao nosso site');
+
+let nome ="Nan";
+
+let idadeI = 25;
+
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+
+let name = prompt('Digite o seu nome: ');
+
+let idade = prompt('Digite a sua idade: ');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}
